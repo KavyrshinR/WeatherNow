be19c5d33dfde2d9c3423406252b20226ad57ec1
Roman <rom.kawirshin@gmail.com>
2017-06-26 23:51:04 +0300

    Добавлены классы AddStationPresenter / View

---
+++ app/src/main/java/ru/kavyrshin/weathernow/presenter/AddStationPresenter.java
@@ -0,0 +1,9 @@
+package ru.kavyrshin.weathernow.presenter;
+
+import ru.kavyrshin.weathernow.view.AddStationView;
+
+
+public class AddStationPresenter extends BasePresenter<AddStationView> {
+
+
+}
---
+++ app/src/main/java/ru/kavyrshin/weathernow/view/AddStationView.java
@@ -0,0 +1,10 @@
+package ru.kavyrshin.weathernow.view;
+
+
+import java.util.List;
+
+import ru.kavyrshin.weathernow.entity.StationListElement;
+
+public interface AddStationView extends BaseView {
+    void showArroundStations(List<StationListElement> arroundStations);
+}
