9b9e5f18feb6de4f031225ed62cfd5c49c4d2172
Roman <rom.kawirshin@gmail.com>
2017-06-27 01:44:56 +0300

    В BaseView добавлен метод showError

--- app/src/main/java/ru/kavyrshin/weathernow/view/BaseView.java
+++ app/src/main/java/ru/kavyrshin/weathernow/view/BaseView.java
@@ -4,7 +4,7 @@ package ru.kavyrshin.weathernow.view;
 import com.arellomobile.mvp.MvpView;
 
 public interface BaseView extends MvpView {
-
+    void showError(String errorMessage);
     void showLoad();
     void hideLoad();
 }
