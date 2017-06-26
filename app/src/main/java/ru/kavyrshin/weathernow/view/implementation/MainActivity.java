ab8e411a2a8b1155fa37136f3f33fdc72aab4374
Roman <rom.kawirshin@gmail.com>
2017-06-27 02:17:01 +0300

    minor

--- app/src/main/java/ru/kavyrshin/weathernow/view/implementation/MainActivity.java
+++ app/src/main/java/ru/kavyrshin/weathernow/view/implementation/MainActivity.java
@@ -1,14 +1,15 @@
 package ru.kavyrshin.weathernow.view.implementation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import ru.kavyrshin.weathernow.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private FloatingActionButton btnAddStation;
 
@@ -23,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         btnAddStation = (FloatingActionButton) findViewById(R.id.btnAddStation);
         stationList = (RecyclerView) findViewById(R.id.stationList);
 
+        btnAddStation.setOnClickListener(this);
+    }
 
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnAddStation) {
+            Intent intent = new Intent(this, AddStationActivity.class);
+            startActivity(intent);
+        }
     }
 }
