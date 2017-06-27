1f78214062e0b37de6b4dd54c7a98d1712394001
Roman <rom.kawirshin@gmail.com>
2017-06-28 02:12:00 +0300

    В MainActivity тулбар заменен на кастомный

--- app/src/main/java/ru/kavyrshin/weathernow/view/implementation/MainActivity.java
+++ app/src/main/java/ru/kavyrshin/weathernow/view/implementation/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import ru.kavyrshin.weathernow.R;
 
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private Toolbar toolbar;
+    private TextView tvToolbar;
 
     private FloatingActionButton btnAddStation;
 
@@ -21,12 +26,26 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvToolbar = (TextView) toolbar.findViewById(R.id.tvOut);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         btnAddStation = (FloatingActionButton) findViewById(R.id.btnAddStation);
         stationList = (RecyclerView) findViewById(R.id.stationList);
 
         btnAddStation.setOnClickListener(this);
+
+        setTitle("Хуй");
     }
 
+    @Override
+    public void setTitle(CharSequence title) {
+        if (getSupportActionBar() != null) {
+            tvToolbar.setText(title);
+        }
+        super.setTitle(title);
+    }
 
     @Override
     public void onClick(View view) {
