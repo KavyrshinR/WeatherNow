f83c58fe2d4dbd585c480426c08fd075ea3088e3
Roman <rom.kawirshin@gmail.com>
2017-06-30 20:06:15 +0300

    Добавлена проверка на подключение к сети

--- app/src/main/java/ru/kavyrshin/weathernow/model/DataManager.java
+++ app/src/main/java/ru/kavyrshin/weathernow/model/DataManager.java
@@ -4,6 +4,7 @@ package ru.kavyrshin.weathernow.model;
 import java.util.List;
 
 import ru.kavyrshin.weathernow.BuildConfig;
+import ru.kavyrshin.weathernow.MyApplication;
 import ru.kavyrshin.weathernow.entity.MainStationModel;
 import ru.kavyrshin.weathernow.entity.StationListElement;
 import ru.kavyrshin.weathernow.model.api.ApiModule;
@@ -36,19 +37,23 @@ public class DataManager {
         Observable<MainStationModel> mainStationObservable = apiWeather.getStationArround(latitude,
                 longitude, ApiModule.countStationArround, ApiModule.units, BuildConfig.API_KEY);
 
-        return mainStationObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .flatMap(new Func1<MainStationModel, Observable<List<StationListElement>>>() {
-                    @Override
-                    public Observable<List<StationListElement>> call(MainStationModel mainStationModel) {
-                        if ((mainStationModel != null) && (!mainStationModel.getList().isEmpty())) {
-                            return Observable.just(mainStationModel.getList());
-                        } else {
-                            RuntimeException runtimeException = new RuntimeException();
-                            return Observable.error(new CustomException(CustomException.SERVER_EXCEPTION, "Ошибка сервера"));
+        if (!MyApplication.isNetworkConnected()) {
+            return Observable.error(new CustomException(CustomException.NETWORK_EXCEPTION, "Нет подключения"));
+        } else {
+            return mainStationObservable
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .flatMap(new Func1<MainStationModel, Observable<List<StationListElement>>>() {
+                        @Override
+                        public Observable<List<StationListElement>> call(MainStationModel mainStationModel) {
+                            if ((mainStationModel != null) && (!mainStationModel.getList().isEmpty())) {
+                                return Observable.just(mainStationModel.getList());
+                            } else {
+                                RuntimeException runtimeException = new RuntimeException();
+                                return Observable.error(new CustomException(CustomException.SERVER_EXCEPTION, "Ошибка сервера"));
+                            }
                         }
-                    }
-                });
+                    });
+        }
     }
 }
