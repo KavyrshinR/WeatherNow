0f54874146eff9da134dc6f73cb56f7abec141dd
Roman <rom.kawirshin@gmail.com>
2017-09-06 01:38:58 +0300

    Исправлены отступы

--- app/src/main/java/ru/kavyrshin/weathernow/view/implementation/adapter/MyStationsAdapter.java
+++ app/src/main/java/ru/kavyrshin/weathernow/view/implementation/adapter/MyStationsAdapter.java
@@ -2,12 +2,15 @@ package ru.kavyrshin.weathernow.view.implementation.adapter;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.kavyrshin.weathernow.R;
 import ru.kavyrshin.weathernow.entity.MainWeatherModel;
 
 
@@ -22,16 +25,21 @@ public class MyStationsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        RecyclerView recyclerView = new RecyclerView(parent.getContext());
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(parent.getContext(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        MyStationViewHolder myStationViewHolder = new MyStationViewHolder(recyclerView);
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.station_list_item, parent, false);
+        MyStationViewHolder myStationViewHolder = new MyStationViewHolder(view);
         return myStationViewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         MyStationViewHolder myStationViewHolder = (MyStationViewHolder) holder;
+        myStationViewHolder.cityName.setText(myStations.get(position).getCity().getName());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(myStationViewHolder.itemView.getContext(),
+                LinearLayoutManager.HORIZONTAL, false);
+        myStationViewHolder.recyclerView.setLayoutManager(linearLayoutManager);
+
         myStationViewHolder.recyclerView.setAdapter(new StationWeatherAdapter(myStationsListener, myStations.get(position)));
     }
 
@@ -50,11 +58,13 @@ public class MyStationsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     static class MyStationViewHolder extends RecyclerView.ViewHolder {
 
+        TextView cityName;
         RecyclerView recyclerView;
 
         public MyStationViewHolder(View itemView) {
             super(itemView);
-            recyclerView = (RecyclerView) itemView;
+            cityName = (TextView) itemView.findViewById(R.id.cityName);
+            recyclerView = (RecyclerView) itemView.findViewById(R.id.weatherList);
         }
 
     }
