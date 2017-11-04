7fbbbe10dcfd5f316ff7f89c6ceedd5da5ea62d4
Roman <rom.kawirshin@gmail.com>
2017-11-05 02:00:48 +0300

    Добавлен объект DataSource

---
+++ app/src/main/java/ru/kavyrshin/weathernow/entity/DataSource.java
@@ -0,0 +1,22 @@
+package ru.kavyrshin.weathernow.entity;
+
+
+public class DataSource {
+
+    public static final int INTERNET_DATA_SOURCE = 123;
+    public static final int DISK_DATA_SOURCE = 1234;
+
+    private int source;
+
+    public DataSource(int source) {
+        this.source = source;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public void setSource(int source) {
+        this.source = source;
+    }
+}
