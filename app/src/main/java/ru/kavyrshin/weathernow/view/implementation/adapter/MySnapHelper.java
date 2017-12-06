a327320f2f32bff24b9f4cafced70f4fc2640a29
Roman <rom.kawirshin@gmail.com>
2017-12-06 09:36:42 +0300

    Исправлен баг в MySnapHelper

--- app/src/main/java/ru/kavyrshin/weathernow/view/implementation/adapter/MySnapHelper.java
+++ app/src/main/java/ru/kavyrshin/weathernow/view/implementation/adapter/MySnapHelper.java
@@ -2,6 +2,7 @@ package ru.kavyrshin.weathernow.view.implementation.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
@@ -11,26 +12,94 @@ import android.view.View;
 class MySnapHelper extends LinearSnapHelper {
 
     @Nullable
+    private OrientationHelper mVerticalHelper;
+    @Nullable
+    private OrientationHelper mHorizontalHelper;
+
+    @Nullable
     @Override
     public int[] calculateDistanceToFinalSnap(@NonNull RecyclerView.LayoutManager layoutManager, @NonNull View targetView) {
         int[] out = new int[2];
         if (layoutManager.canScrollHorizontally()) {
-            out[0] = getDistance(layoutManager, targetView, OrientationHelper.createHorizontalHelper(layoutManager));
+            out[0] = getDistance(layoutManager, targetView, getHorizontalHelper(layoutManager));
         } else {
             out[0] = 0;
         }
 
         if (layoutManager.canScrollVertically()) {
-            out[1] = getDistance(layoutManager, targetView, OrientationHelper.createVerticalHelper(layoutManager));
+            out[1] = getDistance(layoutManager, targetView, getVerticalHelper(layoutManager));
         } else {
             out[1] = 0;
         }
         return out;
     }
 
+    @Override
+    public View findSnapView(RecyclerView.LayoutManager layoutManager) {
+        if (layoutManager instanceof LinearLayoutManager) {
+
+            if (layoutManager.canScrollHorizontally()) {
+                return getStartView(layoutManager, getHorizontalHelper(layoutManager));
+            } else {
+                return getStartView(layoutManager, getVerticalHelper(layoutManager));
+            }
+        }
+
+        return super.findSnapView(layoutManager);
+    }
+
+    private View getStartView(RecyclerView.LayoutManager layoutManager,
+                              OrientationHelper helper) {
+
+        if (layoutManager instanceof LinearLayoutManager) {
+            int firstChild = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+
+            boolean isLastItem = ((LinearLayoutManager) layoutManager)
+                    .findLastCompletelyVisibleItemPosition()
+                    == layoutManager.getItemCount() - 1;
+
+            if (firstChild == RecyclerView.NO_POSITION || isLastItem) {
+                return null;
+            }
+
+            View child = layoutManager.findViewByPosition(firstChild);
+
+            if (helper.getDecoratedEnd(child) >= helper.getDecoratedMeasurement(child) / 2
+                    && helper.getDecoratedEnd(child) > 0) {
+                return child;
+            } else {
+                if (((LinearLayoutManager) layoutManager).findLastCompletelyVisibleItemPosition()
+                        == layoutManager.getItemCount() - 1) {
+                    return null;
+                } else {
+                    return layoutManager.findViewByPosition(firstChild + 1);
+                }
+            }
+        }
+
+        return super.findSnapView(layoutManager);
+    }
+
     private int getDistance(RecyclerView.LayoutManager layoutManager, View targetView, OrientationHelper helper) {
         final int childStart = helper.getDecoratedStart(targetView);
         final int containerStart = helper.getStartAfterPadding();
         return childStart - containerStart;
+    }
+
+    @NonNull
+    private OrientationHelper getVerticalHelper(@NonNull RecyclerView.LayoutManager layoutManager) {
+        if (mVerticalHelper == null) {
+            mVerticalHelper = OrientationHelper.createVerticalHelper(layoutManager);
+        }
+        return mVerticalHelper;
+    }
+
+    @NonNull
+    private OrientationHelper getHorizontalHelper(
+            @NonNull RecyclerView.LayoutManager layoutManager) {
+        if (mHorizontalHelper == null) {
+            mHorizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager);
+        }
+        return mHorizontalHelper;
     }
 }
