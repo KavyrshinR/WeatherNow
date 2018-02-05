d351e8adb13c3051bcdabcf52e12881390be5ce0
Roman <rom.kawirshin@gmail.com>
2018-02-05 16:31:53 +0300

    Реализован метод получения application в BaseActivity

--- app/src/main/java/ru/kavyrshin/weathernow/ui/BaseActivity.java
+++ app/src/main/java/ru/kavyrshin/weathernow/ui/BaseActivity.java
@@ -2,8 +2,17 @@ package ru.kavyrshin.weathernow.ui;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
+import ru.kavyrshin.weathernow.MyApplication;
+
 
 public class BaseActivity extends MvpAppCompatActivity {
 
+    private MyApplication application;
 
+    public MyApplication myApplication() {
+        if (application == null) {
+            application = (MyApplication) getApplication();
+        }
+        return application;
+    }
 }
