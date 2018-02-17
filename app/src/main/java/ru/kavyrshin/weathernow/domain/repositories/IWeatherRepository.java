1aac3e336bf93573f6606a5503c3b74f194424fa
Roman <rom.kawirshin@gmail.com>
2018-02-17 05:41:43 +0300

    Добавлены новые методы в IWeatherRepository

--- app/src/main/java/ru/kavyrshin/weathernow/domain/repositories/IWeatherRepository.java
+++ app/src/main/java/ru/kavyrshin/weathernow/domain/repositories/IWeatherRepository.java
@@ -1,6 +1,17 @@
 package ru.kavyrshin.weathernow.domain.repositories;
 
 
-public interface IWeatherRepository {
+import android.util.Pair;
 
+import java.util.List;
+
+import ru.kavyrshin.weathernow.domain.models.CacheCity;
+import ru.kavyrshin.weathernow.domain.models.DataSource;
+import ru.kavyrshin.weathernow.domain.models.MainWeatherModel;
+import rx.Observable;
+
+public interface IWeatherRepository {
+    Observable<Pair<DataSource, List<MainWeatherModel>>> getWeather(final List<CacheCity> favouriteCities);
+    Observable<Pair<DataSource, List<MainWeatherModel>>> getAllCachedWeather();
+    void deleteWeatherByCityId(int cityId);
 }
