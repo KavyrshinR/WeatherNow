6dfca26ecc4de2d6a6eb1c3e8b76acb37ca7983a
Roman <rom.kawirshin@gmail.com>
2018-02-17 05:42:02 +0300

    Добавлен IStationsRepository

---
+++ app/src/main/java/ru/kavyrshin/weathernow/domain/repositories/IStationsRepository.java
@@ -0,0 +1,14 @@
+package ru.kavyrshin.weathernow.domain.repositories;
+
+import java.util.List;
+
+import ru.kavyrshin.weathernow.domain.models.CacheCity;
+import ru.kavyrshin.weathernow.domain.models.StationListElement;
+import rx.Observable;
+
+public interface IStationsRepository {
+    Observable<List<StationListElement>> getStationsArround(double latitude, double longitude, int countStations);
+    Observable<List<CacheCity>> getFavouriteStations();
+    void saveFavouriteStation(CacheCity cacheCity);
+    void deleteFavouriteStation(int cityId);
+}
