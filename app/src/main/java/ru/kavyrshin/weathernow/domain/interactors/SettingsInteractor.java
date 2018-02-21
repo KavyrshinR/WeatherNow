8e49020d7a3f2e7b59115d8dd947324eb3ba330d
Roman <rom.kawirshin@gmail.com>
2018-02-21 17:43:22 +0300

    Добавлен и реализован SettingsInteractor

---
+++ app/src/main/java/ru/kavyrshin/weathernow/domain/interactors/SettingsInteractor.java
@@ -0,0 +1,33 @@
+package ru.kavyrshin.weathernow.domain.interactors;
+
+import javax.inject.Inject;
+
+import ru.kavyrshin.weathernow.data.repositories.SettingsRepository;
+import ru.kavyrshin.weathernow.util.WeatherSettings;
+import rx.Observable;
+
+public class SettingsInteractor {
+
+    private SettingsRepository settingsRepository;
+
+    @Inject
+    public SettingsInteractor(SettingsRepository settingsRepository) {
+        this.settingsRepository = settingsRepository;
+    }
+
+    public Observable<WeatherSettings> saveTemperatureSettings(@WeatherSettings.TemperatureSettings int temperatureUnit) {
+        return settingsRepository.saveTemperatureSettings(temperatureUnit);
+    }
+
+    public Observable<WeatherSettings> savePressureSettings(@WeatherSettings.PressureSettings int pressureUnit) {
+        return settingsRepository.savePressureSettings(pressureUnit);
+    }
+
+    public Observable<WeatherSettings> saveWindSpeedSettings(@WeatherSettings.SpeedSettings int windSpeedUnit) {
+        return settingsRepository.saveWindSpeedSettings(windSpeedUnit);
+    }
+
+    public Observable<WeatherSettings> getWeatherSettings() {
+        return settingsRepository.getWeatherSettings();
+    }
+}
