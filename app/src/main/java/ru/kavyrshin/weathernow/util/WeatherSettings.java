0feabb9d32fbd14edc1478c45b09c571a181f72b
Roman <rom.kawirshin@gmail.com>
2018-02-21 11:42:16 +0300

    Добавлена intDef типизация в WeatherSettings

--- app/src/main/java/ru/kavyrshin/weathernow/util/WeatherSettings.java
+++ app/src/main/java/ru/kavyrshin/weathernow/util/WeatherSettings.java
@@ -1,17 +1,31 @@
 package ru.kavyrshin.weathernow.util;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
 public class WeatherSettings extends RealmObject {
 
+    @Retention(RetentionPolicy.CLASS)
+    @IntDef({CELSIUS_UNIT, KELVIN_UNIT, FAHRENHEIT_UNIT})
+    public @interface TemperatureSettings{}
     public static final int CELSIUS_UNIT = 11;
     public static final int KELVIN_UNIT = 12;
     public static final int FAHRENHEIT_UNIT = 13;
 
+    @Retention(RetentionPolicy.CLASS)
+    @IntDef({H_PA_UNIT, MM_OF_MERCURY_UNIT})
+    public @interface PressureSettings{}
     public static final int H_PA_UNIT = 21;
     public static final int MM_OF_MERCURY_UNIT = 22;
 
+    @Retention(RetentionPolicy.CLASS)
+    @IntDef({M_PER_SEC_UNIT, MI_PER_HOUR_UNIT})
+    public @interface SpeedSettings{}
     public static final int M_PER_SEC_UNIT = 31;
     public static final int MI_PER_HOUR_UNIT = 32;
 
@@ -23,26 +37,29 @@ public class WeatherSettings extends RealmObject {
     private int windSpeedUnit = M_PER_SEC_UNIT;
 
 
-    public void setTemperatureUnit(int temperatureUnit) {
+    public void setTemperatureUnit(@TemperatureSettings int temperatureUnit) {
         this.temperatureUnit = temperatureUnit;
     }
 
-    public void setPressureUnit(int pressureUnit) {
+    public void setPressureUnit(@PressureSettings int pressureUnit) {
         this.pressureUnit = pressureUnit;
     }
 
-    public void setWindSpeedUnit(int windSpeedUnit) {
+    public void setWindSpeedUnit(@SpeedSettings int windSpeedUnit) {
         this.windSpeedUnit = windSpeedUnit;
     }
 
+    @TemperatureSettings
     public int getTemperatureUnit() {
         return temperatureUnit;
     }
 
+    @PressureSettings
     public int getPressureUnit() {
         return pressureUnit;
     }
 
+    @SpeedSettings
     public int getWindSpeedUnit() {
         return windSpeedUnit;
     }
