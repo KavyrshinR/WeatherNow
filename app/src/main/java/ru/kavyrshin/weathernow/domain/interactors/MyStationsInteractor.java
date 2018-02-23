6cc659f2310167fbd290c9dcceda6627373da0ef
Roman <rom.kawirshin@gmail.com>
2018-02-23 21:44:17 +0300

    Изменен метод получения настроек в MyStationsInteractor

--- app/src/main/java/ru/kavyrshin/weathernow/domain/interactors/MyStationsInteractor.java
+++ app/src/main/java/ru/kavyrshin/weathernow/domain/interactors/MyStationsInteractor.java
@@ -11,6 +11,7 @@ import javax.inject.Inject;
 import ru.kavyrshin.weathernow.domain.models.CacheCity;
 import ru.kavyrshin.weathernow.domain.models.DataSource;
 import ru.kavyrshin.weathernow.domain.models.MainWeatherModel;
+import ru.kavyrshin.weathernow.domain.repositories.ISettingsRepository;
 import ru.kavyrshin.weathernow.domain.repositories.IStationsRepository;
 import ru.kavyrshin.weathernow.domain.repositories.IWeatherRepository;
 import ru.kavyrshin.weathernow.util.Utils;
@@ -23,42 +24,53 @@ public class MyStationsInteractor {
 
     private IWeatherRepository weatherRepository;
     private IStationsRepository stationsRepository;
-    private WeatherSettings weatherSettings;
+    private ISettingsRepository settingsRepository;
 
     @Inject
     public MyStationsInteractor(IWeatherRepository weatherRepository, IStationsRepository stationsRepository,
-                                WeatherSettings weatherSettings) {
+                                ISettingsRepository settingsRepository) {
         this.weatherRepository = weatherRepository;
         this.stationsRepository = stationsRepository;
-        this.weatherSettings = weatherSettings;
+        this.settingsRepository = settingsRepository;
     }
 
     public Observable<Pair<DataSource, List<MainWeatherModel>>> getAllWeather() {
         return stationsRepository.getFavouriteStations()
                 .concatMap(new Func1<List<CacheCity>, Observable<Pair<DataSource, List<MainWeatherModel>>>>() {
             @Override
-            public Observable<Pair<DataSource, List<MainWeatherModel>>> call(List<CacheCity> cacheCities) {
+            public Observable<Pair<DataSource, List<MainWeatherModel>>> call(final List<CacheCity> cacheCities) {
 
-                if (cacheCities.isEmpty()) {
-                    return Observable.just(null);
-                }
+                return settingsRepository.getWeatherSettings()
+                        .flatMap(new Func1<WeatherSettings, Observable<Pair<DataSource, List<MainWeatherModel>>>>() {
+                            @Override
+                            public Observable<Pair<DataSource, List<MainWeatherModel>>> call(final WeatherSettings weatherSettings) {
+                                return weatherRepository.getWeather(cacheCities)
+                                        .startWith(weatherRepository.getAllCachedWeather())
+                                        .subscribeOn(Schedulers.io())
+                                        .filter(new Func1<Pair<DataSource, List<MainWeatherModel>>, Boolean>() {
+                                            @Override
+                                            public Boolean call(Pair<DataSource, List<MainWeatherModel>> dataSourceListPair) {
+                                                return !dataSourceListPair.second.isEmpty();
+                                            }
+                                        })
+                                        .concatMap(new Func1<Pair<DataSource, List<MainWeatherModel>>,
+                                                Observable<Pair<DataSource, List<MainWeatherModel>>>>() {
+                                            @Override
+                                            public Observable<Pair<DataSource, List<MainWeatherModel>>> call(Pair<DataSource, List<MainWeatherModel>> dataSourceListPair) {
 
-                return weatherRepository.getWeather(cacheCities)
-                        .startWith(weatherRepository.getAllCachedWeather())
-                        .concatMap(new Func1<Pair<DataSource, List<MainWeatherModel>>, Observable<Pair<DataSource, List<MainWeatherModel>>>>() {
-                    @Override
-                    public Observable<Pair<DataSource, List<MainWeatherModel>>> call(Pair<DataSource, List<MainWeatherModel>> dataSourceListPair) {
-                        ArrayList<MainWeatherModel> weatherModels = new ArrayList<>(dataSourceListPair.second);
+                                                ArrayList<MainWeatherModel> weatherModels = new ArrayList<>(dataSourceListPair.second);
 
-                        for (MainWeatherModel item : weatherModels) {
-                            Utils.convertWeatherUnit(item, weatherSettings);
-                        }
+                                                for (MainWeatherModel item : weatherModels) {
+                                                    Utils.convertWeatherUnit(item, weatherSettings);
+                                                }
 
-                        return Observable.just(dataSourceListPair);
-                    }
-                });
+                                                return Observable.just(dataSourceListPair);
+                                            }
+                                        });
+                            }
+                        });
             }
-        }).subscribeOn(Schedulers.io());
+        });
     }
 
     public void deleteFavouriteStation(int cityId) {
