c68ff8bbe9e3a256558d5d0a4551ff17ab3f06a6
Roman <rom.kawirshin@gmail.com>
2018-02-23 21:45:10 +0300

    Изменен метод получения настроек в AddStationInteractor

--- app/src/main/java/ru/kavyrshin/weathernow/domain/interactors/AddStationInteractor.java
+++ app/src/main/java/ru/kavyrshin/weathernow/domain/interactors/AddStationInteractor.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import ru.kavyrshin.weathernow.domain.models.CacheCity;
 import ru.kavyrshin.weathernow.domain.models.StationListElement;
+import ru.kavyrshin.weathernow.domain.repositories.ISettingsRepository;
 import ru.kavyrshin.weathernow.domain.repositories.IStationsRepository;
 import ru.kavyrshin.weathernow.util.Utils;
 import ru.kavyrshin.weathernow.util.WeatherSettings;
@@ -16,30 +17,35 @@ import rx.schedulers.Schedulers;
 
 public class AddStationInteractor {
 
-    private WeatherSettings weatherSettings;
     private IStationsRepository stationsRepository;
+    private ISettingsRepository settingsRepository;
 
 
     @Inject
-    public AddStationInteractor(IStationsRepository stationsRepository,
-                                WeatherSettings weatherSettings) {
+    public AddStationInteractor(IStationsRepository stationsRepository, ISettingsRepository settingsRepository) {
         this.stationsRepository = stationsRepository;
-        this.weatherSettings = weatherSettings;
+        this.settingsRepository = settingsRepository;
     }
 
-    public Observable<List<StationListElement>> getStationsArround(double latitude, double longitude) {
-        return stationsRepository.getStationsArround(latitude, longitude, 10)
-                .flatMap(new Func1<List<StationListElement>, Observable<List<StationListElement>>>() {
-                    @Override
-                    public Observable<List<StationListElement>> call(List<StationListElement> stationListElements) {
-                        for (int i = 0; i < stationListElements.size(); i++) {
-                            Utils.convertWeatherUnit(stationListElements.get(i), weatherSettings);
-                        }
+    public Observable<List<StationListElement>> getStationsArround(final double latitude, final double longitude) {
+        return settingsRepository.getWeatherSettings()
+                .flatMap(new Func1<WeatherSettings, Observable<List<StationListElement>>>() {
+            @Override
+            public Observable<List<StationListElement>> call(final WeatherSettings weatherSettings) {
+                return stationsRepository.getStationsArround(latitude, longitude, 10)
+                        .flatMap(new Func1<List<StationListElement>, Observable<List<StationListElement>>>() {
+                            @Override
+                            public Observable<List<StationListElement>> call(List<StationListElement> stationListElements) {
+                                for (int i = 0; i < stationListElements.size(); i++) {
+                                    Utils.convertWeatherUnit(stationListElements.get(i), weatherSettings);
+                                }
 
-                        return Observable.just(stationListElements);
-                    }
-                })
-                .subscribeOn(Schedulers.io());
+                                return Observable.just(stationListElements);
+                            }
+                        })
+                        .subscribeOn(Schedulers.io());
+            }
+        });
     }
 
     public Observable<CacheCity> saveStation(int cityId) {
